11016e069fd45d177c7ee3b109694c95e90eafd4
JORGEVAZQUEZ\Jorge <vazquezj@indiana.edu>
2017-09-19 15:33:31 -0400

    9-19-17 Start Menu added and Font for text.

---
+++ OnTheEdge/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour {
+
+	public GUIStyle myFont;
+
+	void Awake ()
+	{
+		Time.timeScale = 0.0f;
+	}
+
+	void OnGUI ()
+	{
+		GUI.Box (new Rect (156, 25, 500, 400), "");
+		GUI.Label (new Rect (406, 50, 100, 50), "ON THE EDGE", myFont);
+	}
+}
