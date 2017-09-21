d0421e2ed8bbbafa018ceb2386494429e03ea880
JORGEVAZQUEZ\Jorge <vazquezj@indiana.edu>
2017-09-21 09:42:06 -0400

    9-21-17 Start Menu fully functional.

--- OnTheEdge/Assets/Scripts/StartMenu.cs
+++ OnTheEdge/Assets/Scripts/StartMenu.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class StartMenu : MonoBehaviour {
 
 	public GUIStyle myFont;
+	public Texture2D myTexture;
 
 	void Awake ()
 	{
@@ -13,7 +14,14 @@ public class StartMenu : MonoBehaviour {
 
 	void OnGUI ()
 	{
-		GUI.Box (new Rect (240, 80, 500, 400), "");
-		GUI.Label (new Rect (450, 100, 100, 30), "ON THE EDGE", myFont);
+		//Start Menu Box and Text
+		GUI.Box (new Rect (280, 80, 500, 400), "");
+		GUI.Label (new Rect (480, 100, 100, 30), "ON THE EDGE", myFont);
+		//Start Menu Button and Text
+		GUI.Label (new Rect (480, 430, 100, 50), "START!", myFont);
+		if (GUI.Button (new Rect (455, 430, 150, 50), ""))
+		{
+			Destroy (this);
+		}
 	}
 }
