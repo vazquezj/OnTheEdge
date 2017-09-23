f0f5eeb16dee28ed919201342923db5694a3108c
JORGEVAZQUEZ\Jorge <vazquezj@indiana.edu>
2017-09-23 18:56:16 -0400

    9-23-17 GUI Score Manager Added, functions by pressing Enter on NumPad.

---
+++ OnTheEdge/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+	public static int score;
+	public GUISkin myGUISkin;
+
+	void Awake ()
+	{
+		score = 0;
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.KeypadEnter))
+		{
+			score += 1;
+		}
+	}
+
+	void OnGUI ()
+	{
+		GUI.skin = myGUISkin;
+		GUI.Label (new Rect (50, 50, 300, 100), "SCORE: " + score);
+	}
+}
