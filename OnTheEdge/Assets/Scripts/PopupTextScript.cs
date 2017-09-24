f6e6f2399029165b27a634eab6ef6bee96a01977
JORGEVAZQUEZ\Jorge <vazquezj@indiana.edu>
2017-09-23 21:17:48 -0400

    9-23-17 Started work on Damage popup text.

---
+++ OnTheEdge/Assets/Scripts/PopupTextScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupTextScript : MonoBehaviour {
+
+	public Animator animator;
+	private Text popupText;
+
+	void Start ()
+	{
+		AnimatorClipInfo[] clipInfo = animator.GetCurrentAnimatorClipInfo (0);
+		Destroy (gameObject, clipInfo [0].clip.length);
+		popupText = animator.GetComponent<Text> ();
+	}
+
+	public void SetText (string text)
+	{
+		animator.GetComponent<Text>().text = text;
+	}
+}
